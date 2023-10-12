e2151742600279a2d9e43be2176e747b164d453c
chiki <work@chiki.dev>
2023-10-12 22:48:01 +0300

    better

--- lua/chiki/set.lua
+++ lua/chiki/set.lua
@@ -3,6 +3,11 @@ vim.loader.enable()
 vim.g.netrw_browse_split = 0
 vim.api.nvim_command("autocmd FileType * setlocal formatoptions-=ro")
 vim.g.netrw_banner = 0
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.opt.conceallevel = 3
+vim.opt.grepprg = "rg --vimgrep"
+vim.opt.formatoptions = "jcroqlnt" -- tcqj
+vim.opt.grepformat = "%f:%l:%c:%m"
 vim.g.loaded_netrw = 1
 vim.opt.autochdir = true
 vim.g.loaded_netrwPlugin = 1
@@ -27,28 +32,38 @@ vim.opt.signcolumn = "yes"
 vim.opt.smartindent = true
 vim.opt.softtabstop = 4
 vim.opt.splitright = true
-vim.opt.swapfile = false
 vim.opt.tabstop = 4
 vim.opt.termguicolors = true
 vim.opt.timeoutlen = 200
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 vim.opt.updatetime = 50
 vim.opt.lazyredraw = false
+vim.opt.updatetime = 200 -- Save swap file and trigger CursorHold
+vim.opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
+vim.opt.wildmode = "longest:full,full" -- Command-line completion mode
 vim.opt.wrap = false
 vim.opt.hidden = true
+vim.opt.autowrite = true
 vim.diagnostic.config({
-    virtual_text = true,
-    float = {
-        border = "rounded",
-    },
-    update_in_insert = true,
-    underline = true,
-    severity_sort = true,
-    signs = true,
-
+	virtual_text = true,
+	float = {
+		border = "rounded",
+	},
+	update_in_insert = true,
+	underline = true,
+	severity_sort = true,
+	signs = true,
 })
+-- vim.opt.fillchars = {
+-- 	foldopen = "",
+-- 	foldclose = "",
+-- 	foldsep = " ",
+-- 	diff = "╱",
+-- 	eob = " ",
+-- }
+vim.opt.smoothscroll = true
 
 --diagnostic icons
 vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
@@ -59,12 +74,14 @@ vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
 vim.cmd([[
 autocmd BufEnter * if &ft ==# 'help' | wincmd L | endif
 ]])
-vim.o.foldcolumn = '0' -- '0' is not bad
-vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldcolumn = "0" -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.o.foldenable = true
 vim.o.splitbelow = true
 vim.o.splitright = true
 vim.o.numberwidth = 3
+vim.g.markdown_recommended_style = 0
 -- vim.g.vimtex_compiler_method = 'latexrun'
 -- vim.g.vimtex_view_method = 'zathura'
