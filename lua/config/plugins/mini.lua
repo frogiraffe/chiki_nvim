de44709a4e8959b16ac86f6524b2bf18aba2f16d
chiki <work@chiki.dev>
2025-07-29 01:38:40 +0300

    fixed keymaps

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -1,11 +1,26 @@
 return {
 	{
 		"echasnovski/mini.nvim",
+		lazy = true,
 		version = false,
 		config = function()
 			require("mini.operators").setup()
-			require("mini.move").setup()
-			require("mini.ai").setup()
+			require("mini.move").setup({
+				mappings = {
+					-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+					left = "<A-h>",
+					right = "<A-l>",
+					down = "<A-j>",
+					up = "<A-k>",
+
+					-- Move current line in Normal mode
+					line_left = "<A-h>",
+					line_right = "<A-l>",
+					line_down = "<A-j>",
+					line_up = "<A-k>",
+				},
+			})
+			-- require("mini.ai").setup()
 			require("mini.bracketed").setup()
 			require("mini.cursorword").setup()
 			require("mini.icons").setup()
