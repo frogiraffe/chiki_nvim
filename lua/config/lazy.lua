601eac60bae41a4bdfdcc3ddf6f9db9b0d920159
chiki <work@chiki.dev>
2025-07-29 01:39:41 +0300

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,16 +1,16 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -22,9 +22,32 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    { import = "config.plugins" },
-  },
-  install = { colorscheme = { "habamax" } },
-  checker = { enabled = true },
+	spec = {
+		{ import = "config.plugins.aerial" },
+		{ import = "config.plugins.blink" },
+		{ import = "config.plugins.colorizer" },
+		{ import = "config.plugins.comment" },
+		{ import = "config.plugins.conform" },
+		{ import = "config.plugins.leap" },
+		{ import = "config.plugins.lsp" },
+		{ import = "config.plugins.lualine" },
+		{ import = "config.plugins.mini" },
+		{ import = "config.plugins.neodev" },
+		{ import = "config.plugins.nvim-surround" },
+		{ import = "config.plugins.orgmode" },
+		{ import = "config.plugins.persisted" },
+		{ import = "config.plugins.render-markdown" },
+		{ import = "config.plugins.smart-splits" },
+		{ import = "config.plugins.snacks" },
+		{ import = "config.plugins.splitjoin" },
+		{ import = "config.plugins.tabout" },
+		{ import = "config.plugins.theme" },
+		{ import = "config.plugins.toggleterm" },
+		{ import = "config.plugins.treesitter" },
+		{ import = "config.plugins.ultimate-autopairs" },
+		{ import = "config.plugins.vim-repeat" },
+		{ import = "config.plugins.whichkey" },
+	},
+	install = { colorscheme = { "habamax" } },
+	checker = { enabled = true },
 })
