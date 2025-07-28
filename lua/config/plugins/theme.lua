0e246e637ed7e16e44d3f9f5942998f7bbb808e7
chiki <work@chiki.dev>
2025-07-29 01:39:14 +0300

    literally formatting

--- lua/config/plugins/theme.lua
+++ lua/config/plugins/theme.lua
@@ -1,5 +1,10 @@
 return {
-{ "EdenEast/nightfox.nvim", name = "nighftox", priority = 1000, config =function()
-    vim.cmd.colorscheme"carbonfox"
-end,}
+	{
+		"EdenEast/nightfox.nvim",
+		name = "nighftox",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("carbonfox")
+		end,
+	},
 }
