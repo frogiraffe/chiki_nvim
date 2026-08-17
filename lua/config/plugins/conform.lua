3b104e508ba4ad494a32b66e413ad2b80d4f75f2
frogiraffe <work@chiki.dev>
2026-08-18 02:22:59 +0300

    feat(nvim): add SQLFluff formatting

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -16,28 +16,27 @@ return {
 		opts = {
 			notify_on_error = false,
 			format_on_save = function(bufnr)
-				-- Disable "format_on_save lsp_fallback" for languages that don't
-				-- have a well standardized coding style. You can add additional
-				-- languages here or re-enable it for the disabled ones.
 				local disable_filetypes = { c = true, cpp = true }
 				if disable_filetypes[vim.bo[bufnr].filetype] then
 					return nil
-				else
-					return {
-						timeout_ms = 500,
-						lsp_format = "fallback",
-					}
 				end
+				return {
+					timeout_ms = 500,
+					lsp_format = "fallback",
+				}
 			end,
+			formatters = {
+				sqlfluff = {
+					args = { "format", "--dialect=ansi", "-" },
+				},
+			},
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "ruff_format" },
 				rust = { "rustfmt" },
-				-- Conform can also run multiple formatters sequentially
-				-- python = { "isort", "black" },
-				--
-				-- You can use 'stop_after_first' to run the first available formatter from the list
-				-- javascript = { "prettierd", "prettier", stop_after_first = true },
+				sql = { "sqlfluff" },
+				mysql = { "sqlfluff" },
+				plsql = { "sqlfluff" },
 			},
 		},
 	},
